e013b03b3ed0bc709a9401241233b606b5b70d02
David Rochín <jdrc8@hotmail.com>
2019-03-04 17:39:02 -0700

    Cambios en roles
    -Se agregó el Rol de 'Estudiante'.
    -Se añadió la columna 'access_level' (nivel de acceso) a la tabla de Roles.

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -12,24 +12,35 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
+
+        $role = new Role();
+        $role->name = 'student';
+        $role->description = 'Estudiante';
+        $role->access_level = 1;
+        $role->save();
+
         $role = new Role();
         $role->name = 'professor';
         $role->description = 'Profesor';
+        $role->access_level = 2;
         $role->save();
 
         $role = new Role();
         $role->name = 'schoolserv';
         $role->description = 'Servicios Escolares';
+        $role->access_level = 3;
         $role->save();
 
         $role = new Role();
         $role->name = 'coordinator';
         $role->description = 'Coordinador';
+        $role->access_level = 4;
         $role->save();
 
         $role = new Role();
         $role->name = 'admin';
         $role->description = 'Administrador';
+        $role->access_level = 5;
         $role->save();
     }
 }
