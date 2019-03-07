da6c3843e46227f7f44c8bafa7a2f2530115ba71
oswaldo <campeoswaldo@hotmail.com>
2019-03-07 00:04:10 -0700

    ocultar y mostrar componentes segun tipo de estudiante
    Muestra y oculta depende el radiobutton seleccionado de Interno o externo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,33 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
    
-<!-- end document-->
-<div class="container login-container">
+<script src="jquery-1.3.2.min.js" type="text/javascript"></script>   
+<script>
+jQuery(document).ready(function(){
+  $(".oculto").hide();              
+    $(".inf").click(function(){
+          var nodo = $(this).attr("href");  
+ 
+          if ($(nodo).is(":visible")){
+               $(nodo).hide();
+               return false;
+          }else{
+        $(".oculto").hide("slow");                             
+        $(nodo).fadeToggle("slow");
+        return false;
+          }
+    });
+}); 
+function aparecer(){
+    document.getElementById('info4').style.display = "block";
 
+}
+function ocultar(){
+    document.getElementById('info4').style.display = "none";
 
-
-                <div class="row">
+}
+</script>
+<div class="row">
                     <div class="col-md-6 login-form-1">
                         <h3>Registrarse</h3>
                         <form class="form" method="POST" action="{{ route('login') }}">
@@ -61,20 +82,27 @@
                             </div>
                             <div class="form-group">
                                 <p>Selecciona la opción de acuerdo sí eres estudiante del ITLM</p>
+                               
             <label class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="gender" value="option1">
+          <input class="form-check-input" onclick="ocultar()" type="radio" name="gender" value="option1">
           <span class="form-check-label">Interno</span>
         </label>
-        <label class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="gender" value="option2">
+        <div > 
+            <label class="form-check form-check-inline ">
+          <input onclick="aparecer()" href="#info4" class="form-check-input"   type="radio" name="gender" value="option2">
           <span class="form-check-label">Externo</span>
         </label>
     </div>
-    <div class="row">
-          <div class="form-group col-md-6">
+      
+
+        
+
+    </div>
+    <div class="row well oculto" id="info4">
+          <div  class="form-group col-md-6"">
              <input type="email" name="email" id="email" class="form-control input-sm" placeholder="No. Control">
            </div>
-                 <div class="form-group col-md-6">
+                 <div class="form-group col-md-6">   
           <select id="inputState" class="form-control">
             <option>BD1</option>
               <option>BD2</option>
@@ -124,5 +152,6 @@
                     </div>
                 </div>
             </div>
-           
+          
+
     @endsection
