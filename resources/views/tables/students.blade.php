f33c4a7a9ba80021b3b7abba8a8b9bed74c5e209
David Rochín <jdrc8@hotmail.com>
2019-03-09 09:19:19 -0700

    Se quitó la columna 'correo electrónico' de la tabla de estudiantes (solo de la vista, no de la base de datos) ya que no es necesario y hace que no quede espacio para las demás cosas

--- resources/views/tables/students.blade.php
+++ resources/views/tables/students.blade.php
@@ -8,7 +8,6 @@
                 <th>Nombre(s)</th>
                 <th>Carrera</th>
                 <th>Teléfono</th>
-                <th>Correo electrónico</th>
                 <th>Estado</th>
                 <th></th>
             </tr>
@@ -24,7 +23,6 @@
                 <td>{{ $student->first_names }}</td>
                 <td>{{ !is_null($student->career) ? $student->career->short_name : '' }}</td>
                 <td>{{ $student->phone_number }}</td>
-                <td>{{ $student->email }}</td>
                 <td><span class="badge badge-pill badge-{{ $student->isActive() ? 'primary' : 'secondary' }}">{{ $student->isActive() ? 'Activo' : 'Inactivo' }}</span></td>
                 <td><a href="{{ route('students') }}/{{ $student->id }}">Ver alumno</a></td>
             </tr>
