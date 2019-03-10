14e8f9abc10a835c662372019938d98943c79b62
oswaldo <campeoswaldo@hotmail.com>
2019-03-09 22:09:09 -0700

    formato:texto de carta de acreditacion toefl
    ruta
    controlador
    vista
    Texto de ejemplo del formato

---
+++ resources/views/accreditation-toefl.blade.php
@@ -0,0 +1,92 @@
+		@extends('layouts.blank')
+		@section('title', 'Carta de acreditación TOEFL')
+		@section('content')
+
+		<div id="contenido" >
+<div id="encabezado" class="text-right">
+	{{--aqui van las imagenes tambien--}}
+<p>Instituto Tecnológico de Los Mochis</p>
+</div>
+						<p>A QUIEN CORRESPONDA: </p>
+				<div id="texto">
+					<p>La que suscribe Jefa del Departamento de Gestión Tecnológica y Vinculación de este Instituto, hace constar que el (la) alumno (a):</p>
+
+				</div>
+
+				<div id="datosalumnotabla">
+					
+					<table >
+	 <tr>
+	  <td>NOMBRE:</td> <td>GUEVARA SÁNCHEZ OSWALDO</td>
+	 </tr>
+	 <tr>
+	  <td>No. DE CONTROL:</td> <td>14440600</td>
+	 </tr>
+	   <td>CARRERA:</td> <td>INF. INFORMATICA</td>
+	</table>
+
+				</div>
+
+				<div id="texto2">
+				
+				<p>Acreditó la comprensión de artículos técnicos científicos en el idioma Inglés para efectos de titulación.</p>
+				<p>A petición del interesado y para los fines legales que mejor le convengan, se extiende la presente en la Ciudad de Los Mochis, Sinaloa, a los 29 días del mes de octubre del año Dos Mil Dieciocho. </p>	
+				</div>
+				
+				<div id="atentamente" class="text-center" " >
+					<p>	A T E N T A M E N T E</p>
+					<p>Excelencia en Educación Tecnológica®</p>
+					<p>“EL PROGRESO COMO META, LOS PRINCIPIOS COMO GUÍA”</p>
+
+				</div>
+
+				<div id="autoridades">
+				
+					<table >
+				    <tr>
+	                <td>M. en C. CLAUDIA ALARCÓN VALDEZ</td> <td>Vo.Bo. MC. MARIO FLORES LÓPEZ</td>
+	                </tr>
+	                <tr>
+	                <td>JEFA DEL DEPARTAMENTO DE GESTIÓN</td> <td>SUBDIRECTOR DE PLANEACIÓN Y</td>
+	                </tr>
+	                <td> TECNOLÓGICA Y VINCULACIÓN</td> <td>VINCULACIÓN</td>
+	                </table>
+				</div>
+<p>MFL/CAV/blga</p>
+
+<div id="piedepagina" class="text-center">
+	<p>Blvd. Juan de Dios Batiz y 20 de Noviembre</p>
+	<p>C.P. 81259  Los Mochis, Sin.Teléfonos:  (01-668) 812-58-58, 812-59-59 e-mail: dir_mochis@tecnm.mx </p>
+	<p> www.itmochis.edu.mx</p>
+
+{{--aqui van las imagenes tambien--}}
+
+</div>
+</div>
+
+
+
+
+		@endsection
+		{{--@section('scripts')
+
+		<script type="text/javascript">
+
+			//Abrir el documento en modo de impresión
+			$(document).ready( function(){
+				window.print();
+			});
+		</script>
+
+		<style type="text/css">
+			.table-attendance th {
+				font-size: 1.2rem;
+			}
+			.table-attendance td {
+				padding-bottom: 0.3rem;
+				padding-top: 0.3rem;
+				font-size: 1.2rem;
+			}
+		</style>
+
+		@endsection--}}
